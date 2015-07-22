7725b0dbbd2e06784fb64bb7ab12f5a11e4ef7b5
chubuntuarc <arciniega.chubuntu@gmail.com>
2015-07-22 14:21:25 -0600

    Cierre de caja

--- movimientos.php
+++ movimientos.php
@@ -641,6 +641,30 @@ WARNING: Respond.js doesn't work if you view the page via file:// -->
     <form>
     <input type="submit" id="cierre" value="Finalizar">
     </form>
+    <?php 
+    $con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
+    mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
+    $query = "INSERT INTO historico_movimiento SELECT * FROM movimientos where usuario = '$nom'";
+    $resultado = mysql_query($query);
+     ?>
+     <?php 
+    $con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
+    mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
+    $query = "DELETE FROM movimientos WHERE usuario = '$nom'";
+    $resultado = mysql_query($query);
+     ?>
+     <?php 
+    $con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
+    mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
+    $query = "INSERT INTO historico_adicional SELECT * FROM adicionales where usuario = '$nom'";
+    $resultado = mysql_query($query);
+     ?>
+     <?php 
+    $con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
+    mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
+    $query = "DELETE FROM adicionales WHERE usuario = '$nom'";
+    $resultado = mysql_query($query);
+     ?>
 </div>
 
                                  </div></div>
