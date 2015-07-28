0c8763f922db71f3b68e5ebee71baee87861ec6f
chubuntuarc <arciniega.chubuntu@gmail.com>
2015-07-27 18:33:51 -0600

    Fajillas en todas las pantallas

--- canada2.php
+++ canada2.php
@@ -95,7 +95,7 @@ while ($fila = mysql_fetch_array($resultado)) {
                             <div class="media-body"></div></div></div>
                              
                         <div class="row" >
-                        <div class="item col-xs-12 col-lg-8">
+                        <div class="item col-xs-12 col-lg-7">
                             <div class="panel panel-default paper-shadow" data-z="0.5">
                                 <div class="panel-heading">
                                     <h4 class="text-headline margin-none">Compra</h4>
@@ -138,7 +138,7 @@ while ($fila = mysql_fetch_array($resultado)) {
                                  </div>
                                  </div>
                     <div class="row" >
-                        <div class="item col-xs-4 col-lg-4">
+                        <div class="item col-xs-4 col-lg-5">
                             <div class="panel panel-default paper-shadow" data-z="0.5">
                                 <div class="panel-heading">
                                     <h4 class="text-headline margin-none">Desglose</h4>
@@ -150,96 +150,145 @@ while ($fila = mysql_fetch_array($resultado)) {
         <thead id="tabCompra">
             <tr>
                 <th >Denominación</th>
+                <th></th>
                 <th id="cant">Pesos</th>
+                <th></th>
                 <th>Dólares</th>
             </tr>
         </thead>
         <tbody>
         <tr>
             <td>1000.00</td>
+            <td><input type="text" placeholder="0" id="f1" onkeypress="fajilla1000P(this.form)"></td>
+            <input type="hidden" id="fajillaPMill" name="fajillaPMill" value="0" >
             <td><input id="MIL" name="MIL" type="text" placeholder="0" tabindex=4 onkeypress="pesos1000(this.form)"></td>
             <input type="hidden" id="pesoValMil" name="pesoValMil" value="0">
+            <td><input type="text" readonly="" id="f12"></td>
             <td><input type="text" readonly=""></td>
         </tr>
         <tr>
             <td>500.00</td>
-            <td><input id="500" name="500" type="text" placeholder="0" tabindex=5 onkeypress="pesos500(this.form)"></td>
+            <td><input type="text" placeholder="0" id="f2" onkeypress="fajilla500P(this.form)"></td>
+            <input type="hidden" id="fajillaP500" name="fajillaP500" value="0" >
+            <td><input id="500" name="500" type="text" placeholder="0" tabindex=5 onkeypress="pesos500(this.form);"></td>
             <input type="hidden" id="pesoVal500" name="pesoVal500" value="0">
+            <td><input type="text" readonly="" id="f13"></td>
             <td><input type="text" readonly=""></td>
         </tr>
         <tr>
             <td>200.00</td>
+            <td><input type="text" placeholder="0" id="f3" onkeypress="fajilla200P(this.form)"></td>
+            <input type="hidden" id="fajillaP200" name="fajillaP200" value="0" >
             <td><input id="200" name="200" type="text" placeholder="0" tabindex=6 onkeypress="pesos200(this.form)"></td>
             <input type="hidden" id="pesoVal200" name="pesoVal200" value="0">
+            <td><input type="text" readonly="" id="f14"></td>
             <td><input type="text" readonly=""></td>
         </tr>
         <tr>
             <td>100.00</td>
+            <td><input type="text" placeholder="0" id="f4" onkeypress="fajilla100P(this.form)"></td>
+            <input type="hidden" id="fajillaP100" name="fajillaP100" value="0" >
             <td><input id="100" name="100" type="text" placeholder="0" tabindex=7 onkeypress="pesos100(this.form)"></td>
             <input type="hidden" id="pesoVal100" name="pesoVal100" value="0">
+            <td><input type="text" placeholder="0" id="f15" onkeypress="fajilla100D(this.form)"></td>
+            <input type="hidden" id="fajillaD100" name="fajillaD100" value="0" >
             <td><input id="100Dll" type="text" placeholder="0" tabindex=15 onkeypress="dolares100(this.form)"></td>
             <input type="hidden" id="dolarVal100" name="dolarVal100" value="0">
         </tr>
         <tr>
             <td>50.00</td>
+            <td><input type="text" placeholder="0" id="f5" onkeypress="fajilla50P(this.form)"></td>
+            <input type="hidden" id="fajillaP50" name="fajillaP50" value="0" >
             <td><input id="50" name="50" type="text" placeholder="0" tabindex=8 onkeypress="pesos50(this.form)"></td>
             <input type="hidden" id="pesoVal50" name="pesoVal50" value="0">
+            <td><input type="text" placeholder="0" id="f16" onkeypress="fajilla50D(this.form)"></td>
+             <input type="hidden" id="fajillaD50" name="fajillaD50" value="0" >
             <td><input id="50Dll" type="text" placeholder="0" tabindex=16 onkeypress="dolares50(this.form)"></td>
             <input type="hidden" id="dolarVal50" name="dolarVal50" value="0">
         </tr>  
         <tr>
             <td>20.00</td>
+            <td><input type="text" placeholder="0" id="f6" onkeypress="fajilla20P(this.form)"></td>
+            <input type="hidden" id="fajillaP20" name="fajillaP20" value="0" >
             <td><input id="20" name="20" type="text" placeholder="0" tabindex=9 onkeypress="pesos20(this.form)"></td>
             <input type="hidden" id="pesoVal20" name="pesoVal20" value="0">
+            <td><input type="text" placeholder="0" id="f17" onkeypress="fajilla20D(this.form)"></td>
+            <input type="hidden" id="fajillaD20" name="fajillaD20" value="0" >
             <td><input id="20Dll" type="text" placeholder="0" tabindex=17 onkeypress="dolares20(this.form)"></td>
             <input type="hidden" id="dolarVal20" name="dolarVal20" value="0">
         </tr> 
         <tr>
             <td>10.00</td>
+            <td><input type="text" placeholder="0" id="f7" onkeypress="fajilla10P(this.form)"></td>
+            <input type="hidden" id="fajillaP10" name="fajillaP10" value="0" >
             <td><input id="10" name="10" type="text" placeholder="0" tabindex=10 onkeypress="pesos10(this.form)"></td>
             <input type="hidden" id="pesoVal10" name="pesoVal10" value="0">
+            <td><input type="text" placeholder="0" id="f18" onkeypress="fajilla10D(this.form)"</td>
+            <input type="hidden" id="fajillaD10" name="fajillaD10" value="0" >
             <td><input id="10Dll" type="text" placeholder="0" tabindex=18 onkeypress="dolares10(this.form)"></td>
             <input type="hidden" id="dolarVal10" name="dolarVal10" value="0">
         </tr> 
         <tr>
             <td>5.00</td>
+            <td><input type="text" placeholder="0" id="f8" onkeypress="fajilla5P(this.form)"></td>
+            <input type="hidden" id="fajillaP5" name="fajillaP5" value="0" >
             <td><input id="5" name="5" type="text" placeholder="0" tabindex=11 onkeypress="pesos5(this.form)"></td>
             <input type="hidden" id="pesoVal5" name="pesoVal5" value="0">
+            <td><input type="text" placeholder="0" id="f19" onkeypress="fajilla5D(this.form)"></td>
+            <input type="hidden" id="fajillaD5" name="fajillaD5" value="0" >
             <td><input id="5Dll" type="text" placeholder="0" tabindex=19 onkeypress="dolares5(this.form)"></td>
             <input type="hidden" id="dolarVal5" name="dolarVal5" value="0">
 
         </tr> 
         <tr>
             <td>2.00</td>
+            <td><input type="text" placeholder="0" id="f9" onkeypress="fajilla2P(this.form)"></td>
+            <input type="hidden" id="fajillaP2" name="fajillaP2" value="0" >
             <td><input id="2" name="2" type="text" placeholder="0" tabindex=12 onkeypress="pesos2(this.form)"></td>
             <input type="hidden" id="pesoVal2" name="pesoVal2" value="0">
+            <td><input type="text" placeholder="0" id="f20" onkeypress="fajilla2D(this.form)"></td>
+            <input type="hidden" id="fajillaD2" name="fajillaD2" value="0" >
             <td><input id="2Dll" type="text" placeholder="0" tabindex=20 onkeypress="dolares2(this.form)"></td>
             <input type="hidden" id="dolarVal2" name="dolarVal2" value="0">
         </tr> 
         <tr>
             <td>1.00</td>
+            <td><input type="text" placeholder="0" id="f10" onkeypress="fajilla1P(this.form)"></td>
+            <input type="hidden" id="fajillaP1" name="fajillaP1" value="0" >
             <td><input id="1" name="1" type="text" placeholder="0" tabindex=13 onkeypress="pesos1(this.form)"></td>
             <input type="hidden" id="pesoVal1" name="pesoVal1" value="0">
+            <td><input type="text" placeholder="0" id="f21" onkeypress="fajilla1D(this.form)"></td>
+            <input type="hidden" id="fajillaD1" name="fajillaD1" value="0" >
             <td><input id="1Dll" type="text" placeholder="0" tabindex=21 onkeypress="dolares1(this.form)"></td>
             <input type="hidden" id="dolarVal1" name="dolarVal1" value="0">
         </tr> 
          <tr>
             <td>0.50</td>
+            <td><input type="text" placeholder="0" id="f11" onkeypress="fajilla50C(this.form)"></td>
+            <input type="hidden" id="fajillaC50" name="fajillaC50" value="0" >
+            <script language="javascript" type="text/javascript">
+                  </script>
             <td><input id="05" name="05" type="text" placeholder="0" tabindex=14 onkeypress="pesos05(this.form)"></td>
             <input type="hidden" id="pesoVal05" name="pesoVal05" value="0">
+            <td><input type="text" readonly="" id="f22"></td>
             <td><input type="text" readonly="" ></td>
         </tr> 
          <tr>
             <td></td>
+            <td></td>
             <td><input id="totalConv" name="totalConv" type="text" placeholder="Total" readonly></td>
+            <td></td>
             <td><input id="totalDllConv" name="totalDllConv" type="text" placeholder="Total" readonly></td>
         </tr> 
          <tr>
+            <td>Cambio</td>
             <td></td>
             <td><input id="totalDif" name="totalDif" type="text" placeholder="Diferencia" readonly></td>
+            <td></td>
             <td><input id="totalDllDif" type="text" placeholder="Diferencia" readonly></td>
         </tr> 
         <tr>
+            <td></td>
             <td></td>
             <input type="hidden" id="dolaresInsertar" name="dolaresInsertar">
             <input type="hidden" id="cambioInsertar" name="cambioInsertar">
