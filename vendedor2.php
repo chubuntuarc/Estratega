1c68c910960435d1ea085cc97b77e4adba22104a
chubuntuarc <arciniega.chubuntu@gmail.com>
2015-08-01 02:40:13 -0600

    Respaldo

--- vendedor2.php
+++ vendedor2.php
@@ -1,18 +1,4 @@
-<?php session_start();
-$GLOBALS['nom'] = $_SESSION['user'];
-error_reporting(0);
-$host = "localhost";
-$user = "root";
-$pw = "";
-$db = "estra";
-$con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
-mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
-$query = "SELECT nombre from usuarios where usuario = '$nom'";
-$resultado = mysql_query($query);
-while ($fila = mysql_fetch_array($resultado)) { 
-    $GLOBALS['identificar']= $fila[nombre];       }
-
-?>
+<?php include_once("php/sesion.php"); ?>
 <!DOCTYPE html>
 <html class="transition-navbar-scroll top-navbar-xlarge bottom-footer" lang="en">
 <head>
