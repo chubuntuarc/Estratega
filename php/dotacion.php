f72417d38a455f309b7915774e329b0f58e6d82f
chubuntuarc <arciniega.chubuntu@gmail.com>
2015-09-02 16:13:50 -0600

    Arqueo dotacion listo

--- php/dotacion.php
+++ php/dotacion.php
@@ -191,25 +191,25 @@ mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
 //Dólares
 $df100 = $_POST['f100d'];
 $dm100 = $_POST['d100'];
-$d100 = $dm100 + ($df100 * 100);
+$d100 = $dm100 + ($df100 * 100) + $dolares100;
 $df50 = $_POST['f50d'];
 $dm50 = $_POST['d50'];
-$d50 = $dm50 + ($df50 * 100);
+$d50 = $dm50 + ($df50 * 100) + $dolares50;
 $df20 = $_POST['f20d'];
 $dm20 = $_POST['d20'];
-$d20 = $dm20 + ($df20 * 100);
+$d20 = $dm20 + ($df20 * 100) + $dolares20;
 $df10 = $_POST['f10d'];
 $dm10 = $_POST['d10'];
-$d10 = $dm10 + ($df10 * 100);
+$d10 = $dm10 + ($df10 * 100) + $dolares10;
 $df5 = $_POST['f5d'];
 $dm5 = $_POST['d5'];
-$d5 = $dm5 + ($df5 * 100);
+$d5 = $dm5 + ($df5 * 100) + $dolares5;
 $df2 = $_POST['f2d'];
 $dm2 = $_POST['d2'];
-$d2 = $dm2 + ($df2 * 100);
+$d2 = $dm2 + ($df2 * 100) + $dolares2;
 $df1 = $_POST['f1d'];
 $dm1 = $_POST['d1'];
-$d1 = $dm1 + ($df1 * 100);
+$d1 = $dm1 + ($df1 * 100) + $dolares1;
 
 
 if ($tPeso != 0 || $tDollar != 0 || $tCana != 0 || $tEuro != 0) {
@@ -224,3 +224,87 @@ else
 }
 
  ?>
+
+<?php 
+$con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
+mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
+
+//Euros
+$ef100 = $_POST['f100e'];
+$em100 = $_POST['e100'];
+$e100 = $em100 + ($ef100 * 100) + $euros100;
+$ef50 = $_POST['f50e'];
+$em50 = $_POST['e50'];
+$e50 = $em50 + ($ef50 * 100) + $euros50;
+$ef20 = $_POST['f20e'];
+$em20 = $_POST['e20'];
+$e20 = $em20 + ($ef20 * 100) + $euros20;
+$ef10 = $_POST['f10e'];
+$em10 = $_POST['e10'];
+$e10 = $em10 + ($ef10 * 100) + $euros10;
+$ef5 = $_POST['f5e'];
+$em5 = $_POST['e5'];
+$e5 = $em5 + ($ef5 * 100) + $euros5;
+$ef2 = $_POST['f2e'];
+$em2 = $_POST['e2'];
+$e2 = $em2 + ($ef2 * 100) + $euros2;
+$ef1 = $_POST['f1e'];
+$em1 = $_POST['e1'];
+$e1 = $em1 + ($ef1 * 100) + $euros1;
+
+
+if ($tPeso != 0 || $tDollar != 0 || $tCana != 0 || $tEuro != 0) {
+  
+}
+else
+{
+$query = "UPDATE arqueo SET e100 = $e100 , e50 = $e50 , e20 = $e20 , e10 = $e10 , e5 = $e5 , e2 = $e2 , e1 = $e1 where usuario = '$nombre'";
+$resultado = mysql_query($query);
+
+
+}
+
+?>
+
+?>
+
+<?php 
+$con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
+mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
+
+//Canadienses
+$cf100 = $_POST['f100c'];
+$cm100 = $_POST['c100'];
+$c100 = $cm100 + ($cf100 * 100) + $canada100;
+$cf50 = $_POST['f50c'];
+$cm50 = $_POST['c50'];
+$c50 = $cm50 + ($cf50 * 100) + $canada50;
+$cf20 = $_POST['f20c'];
+$cm20 = $_POST['c20'];
+$c20 = $cm20 + ($cf20 * 100) + $canada20;
+$cf10 = $_POST['f10c'];
+$cm10 = $_POST['c10'];
+$c10 = $cm10 + ($cf10 * 100) + $canada10;
+$cf5 = $_POST['f5c'];
+$cm5 = $_POST['c5'];
+$c5 = $cm5 + ($cf5 * 100) + $canada5;
+$cf2 = $_POST['f2c'];
+$cm2 = $_POST['c2'];
+$c2 = $cm2 + ($cf2 * 100) + $canada2;
+$cf1 = $_POST['f1c'];
+$cm1 = $_POST['c1'];
+$c1 = $cm1 + ($cf1 * 100) + $canada1;
+
+
+if ($tPeso != 0 || $tDollar != 0 || $tCana != 0 || $tEuro != 0) {
+  
+}
+else
+{
+$query = "UPDATE arqueo SET c100 = $c100 , c50 = $c50 , c20 = $c20 , c10 = $c10 , c5 = $c5 , c2 = $c2 , c1 = $c1 where usuario = '$nombre'";
+$resultado = mysql_query($query);
+
+
+}
+
+?>
