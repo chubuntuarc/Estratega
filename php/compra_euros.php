7b573b148b4c18764dbfc9d067b500dc67ffe1e6
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2015-09-06 22:32:04 -0600

    Arqueo compra euros
    Listo el arqueo en la sección de compra de euros

--- php/compra_euros.php
+++ php/compra_euros.php
@@ -72,120 +72,122 @@
             }            
 
      ?>
-     <!--Sección en la que se maneja la cantidad de billetes del arqueo-->
+    <!--Sección en la que se maneja la cantidad de billetes del arqueo-->
+
      <?php 
-            //Se obtienen los valores actuales del arqueo en pesos
-            $con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
-            mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
-            $query = "SELECT * FROM calculo_arqueo WHERE divisa = 'pesos' and usuario = '$nom'";
-            $resultado = mysql_query($query);
-            while ($fila = mysql_fetch_array($resultado)) {
-                $GLOBALS['Pesos1000'] = $fila[d1000];
-                $GLOBALS['Pesos500'] = $fila[d500];
-                $GLOBALS['Pesos200'] = $fila[d200];
-                $GLOBALS['Pesos100'] = $fila[d100];
-                $GLOBALS['Pesos50'] = $fila[d50];
-                $GLOBALS['Pesos20'] = $fila[d20];
-                $GLOBALS['Pesos10'] = $fila[d10];
-                $GLOBALS['Pesos5'] = $fila[d5];
-                $GLOBALS['Peso1'] = $fila[d1];
-                $GLOBALS['Centavos50'] = $fila[c5];
-            }
-     ?>
-     <?php 
-            $_SESSION['dolarA'] = $dolarAnt;
-            $dolar = $_POST['dolaresInsertar'];
-            $_SESSION['dolar'] = $_POST['dolaresInsertar'];
-            $difPeso = $_POST['totalDif'];
-            $difDolar = $_POST['totalDllDif'];
-            $diferencia = $_POST['totalDifS'];
+$con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
+mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
+$ven = $_POST['selVendedor'];
+$query = "SELECT * FROM arqueo where usuario = '$nom'";
+$resultado = mysql_query($query);
+while ($fila = mysql_fetch_array($resultado)) {
+    $GLOBALS['pesos1000'] = $fila[p1000];
+    $GLOBALS['pesos500'] = $fila[p500];
+    $GLOBALS['pesos200'] = $fila[p200];
+    $GLOBALS['pesos100'] = $fila[p100];
+    $GLOBALS['pesos50'] = $fila[p50];
+    $GLOBALS['pesos20'] = $fila[p20];
+    $GLOBALS['pesos10'] = $fila[p10];
+    $GLOBALS['pesos5'] = $fila[p5];
+    $GLOBALS['pesos2'] = $fila[p2];
+    $GLOBALS['pesos1'] = $fila[p1];
+    $GLOBALS['pesos05'] = $fila[p05];
+    $GLOBALS['euros100'] = $fila[e100];
+    $GLOBALS['euros50'] = $fila[e50];
+    $GLOBALS['euros20'] = $fila[e20];
+    $GLOBALS['euros10'] = $fila[e10];
+    $GLOBALS['euros5'] = $fila[e5];
+    $GLOBALS['euros2'] = $fila[e2];
+    $GLOBALS['euros1'] = $fila[e1];
+}
+?>
 
-            $arq1000 = $Pesos1000 + $_POST['MIL'] + ($_POST['f1'] * 100);
-            $arq500 = $Pesos500 + $_POST['500'] + ($_POST['f2'] * 100);
-            $arq200 = $Pesos200 + $_POST['200'] + ($_POST['f3'] * 100);
-            $arq100 = $Pesos100 + $_POST['100'] + ($_POST['f4'] * 100);
-            $arq50 = $Pesos50 + $_POST['50'] + ($_POST['f5'] * 100);
-            $arq20 = $Pesos20 + $_POST['20'] + ($_POST['f6'] * 100);
-            $arq10 = $Pesos10 + $_POST['10'] + ($_POST['f7'] * 100);
-            $arq5 = $Pesos5 + $_POST['5'] + ($_POST['f8'] * 100);
-            $arq2 = $Pesos2 + $_POST['2'] + ($_POST['f9'] * 100);
-            $arq1 = $Peso1 + $_POST['1'] + ($_POST['f10'] * 100);
-            $arq05 = $Centavos50 + $_POST['05'] + ($_POST['f11'] * 100);
+<?php 
+$con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
+mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
 
-            //Variables usadas en la pantalla de revisión del arqueo
-            $_SESSION['1000PArqueo'] = $arq1000;
-            $_SESSION['500PArqueo'] = $arq500;
-            $_SESSION['200PArqueo'] = $arq200;
-            $_SESSION['100PArqueo'] = $arq100;
-            $_SESSION['50PArqueo'] = $arq50;
-            $_SESSION['20PArqueo'] = $arq20;
-            $_SESSION['10PArqueo'] = $arq10;
-            $_SESSION['5PArqueo'] = $arq5;
-            $_SESSION['2PArqueo'] = $arq2;
-            $_SESSION['1PArqueo'] = $arq1;
-            $_SESSION['05PArqueo'] = $arq05;
+//Salida Pesos
+$pf1000 = $_POST['fajillaPMill'];
+$pm1000 = $_POST['MIL'];
+$p1000 = $pesos1000 - $pm1000 - ($pf1000 / 1000);
+$pf500 = $_POST['fajillaP500'];
+$pm500 = $_POST['500'];
+$p500 = $pesos500 - $pm500 - ($pf500 / 500);
+$pf200 = $_POST['fajillaP200'];
+$pm200 = $_POST['200'];
+$p200 = $pesos200 - $pm200 - ($pf200 / 200);
+$pf100 = $_POST['fajillaP100'];
+$pm100 = $_POST['100'];
+$p100 = $pesos100 - $pm100 - ($pf100 / 100);
+$pf50 = $_POST['fajillaP50'];
+$pm50 = $_POST['50'];
+$p50 = $pesos50 - $pm50 - ($pf50 / 50);
+$pf20 = $_POST['fajillaP20'];
+$pm20 = $_POST['20'];
+$p20 = $pesos20 - $pm20 - ($pf20 / 20);
+$pf10 = $_POST['fajillaP10'];
+$pm10 = $_POST['10'];
+$p10 = $pesos10 - $pm10 - ($pf10 / 10);
+$pf5 = $_POST['fajillaP5'];
+$pm5 = $_POST['5'];
+$p5 = $pesos5 - $pm5 - ($pf5 / 5);
+$pf2 = $_POST['fajillaP2'];
+$pm2 = $_POST['2'];
+$p2 = $pesos2 - $pm2 - ($pf2 / 2);
+$pf1 = $_POST['fajillaP1'];
+$pm1 = $_POST['1'];
+$p1 = $pesos1 - $pm1 - ($pf1 / 1);
+$pf05 = $_POST['fajillaC50'];
+$pm05 = $_POST['05'];
+$p05 = $pesos05 - $pm05 - ($pf05 / 0.5);
 
-            $con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
-            mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
-            if ($dolarAnt >= $dolar && $diferencia == 0.00 && $difDolar == 0.00) {
-                $query = "UPDATE calculo_arqueo SET d1000 = $arq1000, d500 = $arq500, d200 = $arq200, d100 = $arq100, d50 = $arq50, d20 = $arq20, d10 = $arq10, d5 = $arq5, d2 = $arq2, d1 = $arq1, c5 = $arq05 WHERE divisa = 'pesos' and usuario = '$nom'";
-               $resultado = mysql_query($query);
-            }            
 
-     ?>
-     <?php 
-            //Se obtienen los valores actuales del arqueo en dólares
-            $con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
-            mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
-            $query = "SELECT * FROM calculo_arqueo WHERE divisa = 'dolares' and usuario = '$nom'";
-            $resultado = mysql_query($query);
-            while ($fila = mysql_fetch_array($resultado)) {
-                $GLOBALS['Dolares100'] = $fila[d100];
-                $GLOBALS['Dolares50'] = $fila[d50];
-                $GLOBALS['Dolares20'] = $fila[d20];
-                $GLOBALS['Dolares10'] = $fila[d10];
-                $GLOBALS['Dolares5'] = $fila[d5];
-                $GLOBALS['Dolares2'] = $fila[d2];
-                $GLOBALS['Dolar1'] = $fila[d1];
-            }
-     ?>
-     <?php 
-            $_SESSION['dolarA'] = $dolarAnt;
-            $dolar = $_POST['dolaresInsertar'];
-            $_SESSION['dolar'] = $_POST['dolaresInsertar'];
-            $difPeso = $_POST['totalDif'];
-            $difDolar = $_POST['totalDllDif'];
-            $diferencia = $_POST['totalDifS'];
+if ($pesosAnt >= $peso && $totalCambio == 0.00) {
+    $query = "UPDATE arqueo SET p1000 = $p1000 , p500 = $p500 , p200 = $p200 , p100 = $p100 , p50 = $p50 , p20 = $p20 , p10 = $p10 , p5 = $p5 , p2 = $p2 , p1 = $p1 , p05 = $p05 where usuario = '$nom'";
+   $resultado = mysql_query($query);
+}
+else
+{
+      
+}
 
-            $f15 = $_POST['f15'] * 100;
-            $f16 = $_POST['f16'] * 100;
-            $f17 = $_POST['f17'] * 100;
-            $f18 = $_POST['f18'] * 100;
-            $f19 = $_POST['f19'] * 100;
-            $f20 = $_POST['f20'] * 100;
-            $f21 = $_POST['f21'] * 100;
-            $arq100D = $Dolares100 - $_POST['100Dll'] - $f15;
-            $arq50D = $Dolares50 - $_POST['50Dll'] - $f16;
-            $arq20D = $Dolares20 - $_POST['20Dll'] - $f17;
-            $arq10D = $Dolares10 - $_POST['10Dll'] - $f18;
-            $arq5D = $Dolares5 - $_POST['5Dll'] - $f19;
-            $arq2D = $Dolares2 - $_POST['2Dll'] - $f20;
-            $arq1D = $Dolar1 - $_POST['1Dll'] - $f21;
+ ?>
 
-            //Variables usadas en la pantalla de revisión del arqueo
-            $_SESSION['100DArqueo'] = $arq100D;
-            $_SESSION['50DArqueo'] = $arq50D;
-            $_SESSION['20DArqueo'] = $arq20D;
-            $_SESSION['10DArqueo'] = $arq10D;
-            $_SESSION['5DArqueo'] = $arq5D;
-            $_SESSION['2DArqueo'] = $arq2D;
-            $_SESSION['1DArqueo'] = $arq1D;
+ <?php 
+$con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
+mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
 
-            $con = mysql_connect($host,$user,$pw) or die ("No se pudo establecer la conexión");
-            mysql_select_db($db, $con) or die ("No se pudo conectar a la base de datos");
-            if ($dolarAnt >= $dolar && $diferencia == 0.00 && $difDolar == 0.00) {
-                $query = "UPDATE calculo_arqueo SET d100 = $arq100D, d50 = $arq50D, d20 = $arq20D, d10 = $arq10D, d5 = $arq5D,d2 = $arq2D, d1 = $arq1D WHERE divisa = 'dolares' and usuario = '$nom'";
-               $resultado = mysql_query($query);
-            }            
-     ?>
-     
+//Entrada de Dólares
+$df100 = $_POST['f15'];
+$dm100 = $_POST['d100Dll'];
+$d100 = $euros100 + $dm100 + ($df100 * 100) - $_POST['f25'] - $_POST['m100S'];
+$df50 = $_POST['f16'];
+$dm50 = $_POST['d50Dll'];
+$d50 = $euros50 + $dm50 + ($df50 * 100) - $_POST['f26'] - $_POST['m50S'];
+$df20 = $_POST['f17'];
+$dm20 = $_POST['d20Dll'];
+$d20 = $euros20 + $dm20 + ($df20 * 100) - $_POST['f27'] - $_POST['m20S'];
+$df10 = $_POST['f18'];
+$dm10 = $_POST['d10Dll'];
+$d10 = $euros10 + $dm10 + ($df10 * 100) - $_POST['f28'] - $_POST['m10S'];
+$df5 = $_POST['f19'];
+$dm5 = $_POST['d5Dll'];
+$d5 = $euros5 + $dm5 + ($df5 * 100) - $_POST['f29'] - $_POST['m5S'];
+$df2 = $_POST['f20'];
+$dm2 = $_POST['d2Dll'];
+$d2 = $euros2 + $dm2 + ($df2 * 100) - $_POST['f30'] - $_POST['m2S'];
+$df1 = $_POST['f21'];
+$dm1 = $_POST['d1Dll'];
+$d1 = $euros1 + $dm1 + ($df1 * 100) - $_POST['f31'] - $_POST['m1S'];
+
+
+if ($pesosAnt >= $peso && $totalCambio == 0.00) {
+    $query = "UPDATE arqueo SET e100 = $d100 , e50 = $d50 , e20 = $d20 , e10 = $d10 , e5 = $d5 , e2 = $d2 , e1 = $d1 where usuario = '$nom'";
+   $resultado = mysql_query($query);  
+}
+else
+{
+        
+}
+
+ ?>          
